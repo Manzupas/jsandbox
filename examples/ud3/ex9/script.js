c717a706721aa9540ba52a974c53cc940e65edd8
Emilio Eguizábal <9528006+Manzupas@users.noreply.github.com>
2023-11-06 17:27:05 +0100

    Funcionando

--- examples/ud3/ex9/script.js
+++ examples/ud3/ex9/script.js
@@ -1,4 +1,6 @@
 var carta = null;
+let aciertos=0;
+let errores=0;
 
 function compararCarta(cartaNueva) {
     if (carta == null) {
@@ -10,12 +12,18 @@ function compararCarta(cartaNueva) {
         //marcar ambas cartas como correctas
         carta.classList.add('correcta');
         cartaNueva.classList.add('correcta');
+        aciertos++;
+
     } else {
         //quitamos la clase girada
         carta.classList.remove('girada');
         cartaNueva.classList.remove('girada');
+        errores++;
     }
     carta = null;
+    console.log(aciertos);
+    console.log(errores);
+    establecerAciertosErrores();
 }
 
 /**
@@ -27,4 +35,22 @@ function girarCarta(cartaNueva) {
         //OJO. si pongo compararCarta(cartaNueva) se ejecuta directamente, si lo piensas tiene sentido.
         setTimeout(compararCarta, 500, cartaNueva);
     }
+}
+
+function resetear(){
+    aciertos=0;
+    errores=0;
+    var cartas = document.getElementsByClassName("carta")
+    for (var i = 0; i < cartas.length; i++) {
+        cartas.item(i).classList.remove('girada');
+        cartas.item(i).classList.remove('correcta');
+
+    }
+
+    establecerAciertosErrores();
+}
+
+function establecerAciertosErrores(){
+    document.getElementById("aciertos").innerHTML = aciertos;
+    document.getElementById("errores").innerHTML = errores;
 }
